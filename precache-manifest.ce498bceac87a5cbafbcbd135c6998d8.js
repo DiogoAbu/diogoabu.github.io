1e3785ee0097b7bc56c0459bf11d662f67c26ff0
DiogoAbu <diogodeazevedosilva@gmail.com>
2020-11-12 14:48:24 -0300

    Updates

---
+++ precache-manifest.ce498bceac87a5cbafbcbd135c6998d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d68f9a81a3cf37850d6f",
+    "url": "/static/css/main.503fb447.chunk.css"
+  },
+  {
+    "revision": "d68f9a81a3cf37850d6f",
+    "url": "/static/js/main.af086960.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f9498dfaab8ca165f5a3",
+    "url": "/static/js/2.2bbd3f0e.chunk.js"
+  },
+  {
+    "revision": "923567d12264a0aa078902a0e904c21a",
+    "url": "/index.html"
+  }
+];
